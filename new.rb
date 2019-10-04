cf157b3fc58637f6cd893a76adf22e0c661f05e1
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-10-04 09:11:00 +0000

    Automatically backed up by Learn

---
+++ new.rb
@@ -0,0 +1,33 @@
+name = "Alice"
+ 
+if name == "Alice"
+  puts "Hello, Alice!"
+elsif name == "The White Rabbit"
+  puts "Don't be late, White Rabbit"
+elsif name == "The Mad Hatter"
+  puts "Welcome to the tea party, Mad Hatter"
+elsif name == "The Queen of Hearts"
+  puts "Please don't chop off my head!"
+else
+  puts "Whoooo are you?"
+end 
+
+name = "Alic"
+
+case name 
+when "Alice"
+  puts "Hello, Alice!"
+when "The White Rabbit"
+  puts "Don't be late, White Rabbit"
+when "The Mad Hatter"
+  puts "Welcome to the tea party, Mad Hatter"
+when"The Queen of Hearts"
+  puts "Please don't chop off my head!"
+else
+  puts "Whoooo are you?"
+end 
+
+ while 2==1+1 do 
+   puts "nice job Bro!!!!!!"
+ end
+ 
---
+++ test.rb
@@ -0,0 +1,5 @@
+favorite_number = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+10.times do 
+  favorite_number = favorite_number + 1
+end
+puts "And now your favorite_number is 10 more! It is #{favorite_number}"
